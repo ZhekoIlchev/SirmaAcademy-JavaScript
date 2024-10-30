9804d101247c4afe5481ec46f6ce838b7e71a9c5
Zheko Ilchev <Jeko.Ilchev@gmail.com>
2024-10-30 23:18:58 +0200

    Add task 01 - Strings.

---
+++ 11_Strings/task-01-print-characters.js
@@ -0,0 +1,11 @@
+let word1 = 'AWord';
+for (const character of word1) {
+    console.log(character)
+}
+
+console.log('---');
+
+let word2 = 'Sentence'
+for (const character of word2) {
+    console.log(character)
+}
